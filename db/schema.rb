2e5a6d9defb2e3dc29a5af8e3a7429407d2c5ce3
Tony Nguyen <tcv.nguyen.2013@gmail.com>
2019-05-16 19:05:14 -0500

    Add column quantity to table stuffed_animals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_233028) do
+ActiveRecord::Schema.define(version: 2019_05_17_000203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_233028) do
     t.decimal "sale_price", default: "0.0"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "quantity", default: 0
   end
 
 end
--- spec/factories/stuffed_animals.rb
+++ spec/factories/stuffed_animals.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :stuffed_animal do
     name { Faker::Lorem.word }
-    cost { "9.99" }
-    sale_price { "9.99" }
+    cost { Faker::Number.decimal(2) }
+    sale_price { Faker::Number.decimal(2) }
+    quantity { Faker::Number.number(3) }
   end
 end
